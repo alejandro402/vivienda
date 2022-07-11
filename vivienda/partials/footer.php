9762629c753c99577fd0b1aee6e52b4e00425745
Alejandro <62778102+alejandro402@users.noreply.github.com>
2022-07-11 15:59:04 -0500

    Add files via upload

---
+++ vivienda/partials/footer.php
@@ -0,0 +1,134 @@
+<section class="bg-color5">
+    <!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Sección: Social media -->
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom bg-color5"
+  >
+    <!-- Izquierda -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Redes sociales:</span>
+    </div>
+    <!-- Izquierda -->
+
+    <!-- Derecha -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Derecha -->
+  </section>
+  <!-- Sección: Social media -->
+
+  <!-- Sección: Links  -->
+  <section class="bg-color5">
+    <div class="container text-center text-md-start">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Vivienda Raiz
+          </h6>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias, dolorum. Rerum reprehenderit possimus necessitatibus ex?
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Productos
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Casa</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Apartamento</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Renta</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Comercial</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Ciudad
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Cali</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Medellin</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Bogota</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Barranquilla</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contact
+          </h6>
+          <p><i class="fas fa-home me-3"></i> Cali 3013777025</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            info@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> alejandro.marin00@usc.edu.co</p>
+          <p><i class="fas fa-print me-3"></i> +57 3013777025</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Sección: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4 bg-color5" >
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://www.mundomotorcol.com/">www.mundomotorcol.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+</section>
+    
+    
+        
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
---
+++ vivienda/view/index.php
@@ -0,0 +1,315 @@
+<?php 
+
+    include_once("../partials/head.php");
+    include_once("../partials/menu.php");
+    
+
+
+?>
+
+
+<div class="container">
+
+    <div class="row mt-5">
+
+        <div class="col-sm-12 col-md-6 .color3 mt-5">
+            <h1 class="text-center color3">Compra tu propiedad</h1>
+            <p class="text-center color4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum necessitatibus aliquam inventore voluptatibus neque commodi nihil labore quod blanditiis. Unde nam cupiditate facere doloremque ducimus quidem, excepturi voluptatem est maxime.</p>
+        
+        </div>
+        <div class="col-sm-12 col-md-6 .bg-color5">
+            <img src="../img/bg-principal.jpg" alt="" class="img-fluid">
+        </div>
+
+    </div>
+
+    <div class="row mt-5 text-center">
+
+        <div class="col-sm-12 col-md-2">
+            <div class="card shadow p-3 mb-5 bg-body rounded">
+                <div class="card-body">
+                    <a href="casa" class="color4 h2">
+                        <i class="fa-solid fa-house"></i>
+                    </a>
+                    <h4 class="mt-1">Casa</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-2">
+            <div class="card shadow p-3 mb-5 bg-body rounded">
+                <div class="card-body">
+                    <a href="casa" class="color4 h2">
+                        <i class="fa-solid fa-house"></i>
+                    </a>
+                    <h4 class="mt-1">Apartamento</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-2">
+            <div class="card shadow p-3 mb-5 bg-body rounded">
+                <div class="card-body">
+                    <a href="casa" class="color4 h2">
+                        <i class="fa-solid fa-house"></i>
+                    </a>
+                    <h4 class="mt-1">Comercial</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-2">
+            <div class="card shadow p-3 mb-5 bg-body rounded">
+                <div class="card-body">
+                    <a href="casa" class="color4 h2">
+                        <i class="fa-solid fa-house"></i>
+                    </a>
+                    <h4 class="mt-1">Renta</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-2">
+            <div class="card shadow p-3 mb-5 bg-body rounded">
+                <div class="card-body">
+                    <a href="casa" class="color4 h2">
+                        <i class="fa-solid fa-house"></i>
+                    </a>
+                    <h4 class="mt-1">Nuevos</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-2">
+            <div class="card shadow p-3 mb-5 bg-body rounded">
+                <div class="card-body">
+                    <a href="casa" class="color4 h2">
+                        <i class="fa-solid fa-house"></i>
+                    </a>
+                    <h4 class="mt-1">Otras</h4>
+                </div>
+            </div>
+        </div>
+        
+
+
+    </div>
+
+
+    <!-- SECCION DE COMPRA RENTA -->
+
+    <div class="row">
+
+        <div class="col-sm-12 col-md-4">
+            <div class="card shadow p-3 mb-5 bg-body rounded text-center">
+                <img src="../img/img1.jpg" alt="" class="img-top">
+                <div class="card-body">
+                    <h2 class="text-center color3">Comprar propiedad</h2>
+                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, officiis!</p>
+                    <a href="casa" class="btn btn-outline-success">Encontrar Casa</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-4">
+            <div class="card shadow p-3 mb-5 bg-body rounded text-center">
+                <img src="../img/img1.jpg" alt="" class="img-top">
+                <div class="card-body">
+                    <h2 class="text-center color3">Comprar propiedad</h2>
+                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, officiis!</p>
+                    <a href="casa" class="btn btn-outline-success">Encontrar Casa</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-4">
+            <div class="card shadow p-3 mb-5 bg-body rounded text-center">
+                <img src="../img/img1.jpg" alt="" class="img-top">
+                <div class="card-body">
+                    <h2 class="text-center color3">Comprar propiedad</h2>
+                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, officiis!</p>
+                    <a href="casa" class="btn btn-outline-success">Encontrar Casa</a>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+
+    <!-- OFERTAS -->
+
+    <div class="row mt-4">
+        <h1 class="color4 mb-2">Ofertas</h1>
+        
+        <div class="col-sm-12 col-md-3">
+            <div class="card shadow mb-5 bg-body rounded">
+                <img src="../img/interior1.jpg" alt="" class="img-top rounded">
+                <div class="card-body">
+                    <h3 class="color1">Renta</h3>
+                    <p class="color3">4 habitaciones | 67mts2</p>
+                    <p class="color3">Direccion</p>
+                    <i class="fa-solid fa-money-bill-1"> 1200$</i>
+                    <hr>
+                    <i class="fa-solid fa-bed"> 4</i>
+                    <i class="fa-solid fa-bath"> 2</i>
+                    <i class="fa-solid fa-car"> 3</i>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-sm-12 col-md-3">
+            <div class="card shadow mb-5 bg-body rounded">
+                <img src="../img/interior2.jpg" alt="" class="img-top rounded">
+                <div class="card-body">
+                    <h3 class="color1">Renta</h3>
+                    <p class="color3">4 habitaciones | 67mts2</p>
+                    <p class="color3">Direccion</p>
+                    <i class="fa-solid fa-money-bill-1"> 1200$</i>
+                    <hr>
+                    <i class="fa-solid fa-bed"> 4</i>
+                    <i class="fa-solid fa-bath"> 2</i>
+                    <i class="fa-solid fa-car"> 3</i>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-sm-12 col-md-3">
+            <div class="card shadow mb-5 bg-body rounded">
+                <img src="../img/interior3.jpg" alt="" class="img-top rounded">
+                <div class="card-body">
+                    <h3 class="color1">Renta</h3>
+                    <p class="color3">4 habitaciones | 67mts2</p>
+                    <p class="color3">Direccion</p>
+                    <i class="fa-solid fa-money-bill-1"> 1200$</i>
+                    <hr>
+                    <i class="fa-solid fa-bed"> 4</i>
+                    <i class="fa-solid fa-bath"> 2</i>
+                    <i class="fa-solid fa-car"> 3</i>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-3">
+            <div class="card shadow mb-5 bg-body rounded">
+                <img src="../img/interior4.jpg" alt="" class="img-top rounded">
+                <div class="card-body">
+                    <h3 class="color1">Renta</h3>
+                    <p class="color3">4 habitaciones | 67mts2</p>
+                    <p class="color3">Direccion</p>
+                    <i class="fa-solid fa-money-bill-1"> 1200$</i>
+                    <hr>
+                    <i class="fa-solid fa-bed"> 4</i>
+                    <i class="fa-solid fa-bath"> 2</i>
+                    <i class="fa-solid fa-car"> 3</i>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <!-- AÑADIDOS HOY CASAS , APARTAMENTOS -->
+    
+    
+    <div class="row mt-4 mb-4">
+        <div class="col-sm-12 mb-4">
+            <h1 class="color4">Añadidos Hoy
+
+                <a href="" class="btn btn-outline-success">Casa</a>
+                <a href="" class="btn btn-outline-success">Apartamento</a>
+                <a href="" class="btn btn-outline-success">Habitaciones</a>
+                <a href="" class="btn btn-outline-success">Comercial</a>
+            </h1>
+        </div>
+        
+        <div class="col-sm-12 col-md-6">
+            <img src="../img/exterior1.jpg" alt="" class="img-fluid" >
+        </div>
+
+        <div class="col-sm-12 col-md-6">
+            <img src="../img/exterior1.jpg" alt="" class="img-fluid">
+        </div>
+
+    </div>
+
+
+
+    <!-- CALCULO DE COSTOS -->
+
+    <div class="row mt-4 d-flex align-items-center">
+
+        <div class="col-sm-12 col-md-6">
+            <img src="../img/calculadora.jpg" alt="" class="img-fluid">
+        </div>
+
+        <div class="col-sm-12 col-md-6 text-center">
+            <h1 class="text-center color3">Calcula el costo de la propiedad</h1>
+            <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, adipisci recusandae quam vero minus, eligendi fugiat impedit optio exercitationem ratione accusantium quos? Deserunt, eum error dolorum accusamus voluptatem qui minus?</p>
+            <a href="" class="btn btn-outline-success rounded">Calcular</a>
+        </div>
+
+    </div>
+
+
+    <!-- BUSCAR POR CIUDAD -->
+
+    <div class="row mt-5">
+        <h1 class="color4 mt-4">Buscar por ciudad</h1>
+
+        <div class="col-sm-12 col-md-3 rounded-4 mt-4">
+            <div class="card shadow mb-5 bg-body rounded">
+                <img src="../img/ciudad1.jpg" alt="" class="img-fluid rounded" style="height: 14rem;">
+                <div class="card-body">
+                    <h4 class="text-center color4">Cali</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-3 rounded-4 mt-4">
+            <div class="card shadow mb-5 bg-body rounded">
+                <img src="../img/ciudad2.jpg" alt="" class="img-fluid rounded" style="height: 14rem;">
+                <div class="card-body">
+                    <h4 class="text-center color4">Medellin</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-3 rounded-4 mt-4">
+            <div class="card shadow mb-5 bg-body rounded">
+                <img src="../img/ciudad3.jpg" alt="" class="img-fluid rounded" style="height: 14rem;">
+                <div class="card-body">
+                    <h4 class="text-center color4">Bogotá</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-3 rounded-4 mt-4">
+            <div class="card shadow mb-5 bg-body rounded">
+                <img src="../img/ciudad4.jpg" alt="" class="img-fluid rounded" style="height: 14rem;">
+                <div class="card-body">
+                    <h4 class="text-center color4">Barranquilla</h4>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+
+    
+
+
+    <br><br><br><br><br>
+</div>
+
+
+<?php 
+
+
+include_once("../partials/footer.php");
+
+
+?>
